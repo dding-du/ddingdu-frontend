a7e6ce85158a468f518b1c39d3248f12ff38a962
si14444 <si14444@naver.com>
2025-11-21 08:10:03 +0900

    fix: 타임아웃 시간 변경

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -54,7 +54,7 @@ export const tokenManager = {
 
 const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  timeout: 10000,
+  timeout: 60000,
   headers: {
     "Content-Type": "application/json",
   },
